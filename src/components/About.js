707da213f15255ab1e08eb1e66c112ac3073ae91
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-04-08 22:14:01 +0300

    tests

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,20 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <>
+      <div>About</div>
+      <AboutUs image={image} />
+    </>
+  );
 }
-
+function AboutUs(props) {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>this is good</p>
+      <img src={props.image} alt="I made this"></img>
+    </div>
+  );
+}
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,21 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
-  // update the JSX being returned!
-  return <div>Home</div>;
+  return (
+    <div>
+      <HomeWork name={name} city={city} />
+    </div>
+  );
+}
+
+function HomeWork(props) {
+  return (
+    <div id="home">
+      <h1 style={{ color: "firebrick" }}>
+        {props.name} is a Web Developer from {props.city}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
